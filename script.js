19bb3cc7be693b1ee9ef990967e8a276682b7659
khazaimaArshad <160453468+khazaimaArshad@users.noreply.github.com>
2024-02-20 09:53:36 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function mybtn(){
+  alert('Hello Arshad! ');
+}
+function myFun(){
+  alert('Hello!');
+}
+function mytask(){
+  alert('Please enter your valid email or phone no');
+}
+function myname(){
+  alert('Password must be strong');
+}
+function mypswrd(){
+  alert('Please enter your new password')
+}
+function mywork(){
+  alert('Wellcome to facebook new user');
+}
